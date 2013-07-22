519888f274374dc292b8d8701c905a5280d4a757
fagostini <processor@gians-beast.(none)>
2013-07-22 17:46:35 +0200

    division by zero

--- runlibrary.sh
+++ runlibrary.sh
@@ -2,7 +2,7 @@
 
 cd   tmp/$1
 
-	rna=`cat $2 | awk -F "\t" 'BEGIN{s=0}{c=0;for(i=1;i<=length($2);i++){if(substr($2,i,1)~/A/ || substr($2,i,1)~/T/ || substr($2,i,1)~/U/ || substr($2,i,1)~/C/ || substr($2,i,1)~/G/){c++}} s+=c/length($2)}END{if(s/NR==1){print 1}else{print 0}}'`
+	rna=`cat $2 | awk 'BEGIN{s=0}{if(NF==2){c=0;for(i=1;i<=length($2);i++){if(substr($2,i,1)~/A/ || substr($2,i,1)~/T/ || substr($2,i,1)~/U/ || substr($2,i,1)~/C/ || substr($2,i,1)~/G/){c++}} s+=c/length($2)}}END{if(s/NR==1){print 1}else{print 0}}'`
 	prot=`echo $rna | awk '{print 1-$1}'`
 
 	if [[ "$rna" -ne 0 && "$prot" -ne 0 ]]; then
